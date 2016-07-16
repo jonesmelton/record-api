a7bbbae4dd73f79771a7c0ed498c0caaa2969050
Jones Melton <shavah@gmail.com>
2016-07-16 13:56:05 -0500

    Add comment for .pipe_cleaner

--- csv_operations.rb
+++ csv_operations.rb
@@ -6,6 +6,9 @@ module CSVParser
     CSV.new(clean_pipes(csv), headers: true, header_converters: :symbol).map { |row| row.to_hash }
   end
 
+  # following intended to be private
+  # if that were allowed in modules
+
   def self.clean_pipes(csv)
     csv.gsub("|", ",")
   end

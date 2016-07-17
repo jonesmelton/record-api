9a3ebcf67671248597573bbe2fec3270fabfe2a9
Jones Melton <shavah@gmail.com>
2016-07-17 13:17:16 -0500

    Add sort_by_gender method

--- record_collection.rb
+++ record_collection.rb
@@ -1,4 +1,6 @@
 require_relative 'csv_operations'
+require_relative 'record'
+require 'pry'
 
 class RecordCollection
   attr_reader :all_records
@@ -7,7 +9,18 @@ class RecordCollection
     @all_records = load_records
   end
 
+  def order_by_gender
+    split_genders = all_records.group_by { |record| record.gender }
+    split_and_sorted = split_genders.map { |gender, records| sort_by_last_name(split_genders[gender]) }
+    split_and_sorted["female"].concat(split_and_sorted["male"])
+    p split_and_sorted
+  end
+
   private
+
+  def sort_by_last_name(collection)
+    collection.sort_by { |record| record.last_name }
+  end
 
   def load_records
     raw_csv = File.read("db/records.csv")
--- spec/record_collection_spec.rb
+++ spec/record_collection_spec.rb
@@ -16,4 +16,15 @@ describe RecordCollection do
       expect(test_records.all_records).to all(be_a Record)
     end
   end
+
+  describe "sorting" do
+
+    describe "#order_by_gender" do
+      it "returns females first" do
+        expect(test_records.order_by_gender.map { |record| record.first_name } ).to eq(["Laeticia", "Mary", "Jason"])
+      end
+    end
+
+  end
+
 end
